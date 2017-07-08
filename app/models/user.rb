52ee73811240620e8ef9f2e101cc6f17c449beb8
Smagina Marina <pandamarina736@gmail.com>
2017-07-08 12:15:03 +0600

    Добавлен devise и пользователи

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :notices, dependent: :destroy
   has_many :books, dependent: :destroy
+
+  def role?(r)
+    role.include? r.to_s
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :categories
   resources :notices
+  root to: "books#index"
   resources :books
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
